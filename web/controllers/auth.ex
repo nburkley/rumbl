1139a0c459caeeb8e06dee65acdeb510e1dde59e
Niall Burkley <niallburkley@gmail.com>
2016-07-13 18:09:47 +0200

    Adding authentication tests for video routes

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -10,6 +10,7 @@ defmodule Rumbl.Auth do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
+
     user    = user_id && repo.get(Rumbl.User, user_id)
     assign(conn, :current_user, user)
   end
